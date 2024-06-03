a31ceba4d6d3d50ae9c4ccf831aa6baf0b725ad3
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-06-03 19:21:10 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -3,18 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const adj = ["great", "big", "kind", "pretty"];
   const noun = ["jogger", "racoon", "dog", "cat", "opossum"];
 
-  let phrases = [];
+  let domains = [];
 
-  // Triple bucle anidado para combinar todos los elementos
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        phrases.push(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+        domains.push(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
       }
     }
   }
 
-  // Mostrar las frases generadas en la página web
   const output = document.getElementById("output");
-  output.innerHTML = phrases.join("<br>");
+  output.innerHTML = domains.join("<br>");
 });
